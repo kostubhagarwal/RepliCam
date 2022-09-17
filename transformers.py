f18b83ae822787830488021ed76f19582f67daae
SubwayMan <tyche4490@gmail.com>
2022-09-17 18:26:03 -0400

    scaffolding

--- transformers.py
+++ transformers.py
@@ -1,5 +1,8 @@
 
 
-def stl_ify(filename: str):
+def stlify(filename: str):
     """Function that takes in a LAS file and turns it into an STL file."""
+
+def gcodeify(filename: str):
+    """Function that reads a STL file from disk, then saes as gcode."""
 
