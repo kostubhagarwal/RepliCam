bba56d0d714309b908c9ff88b8bd03069ca05602
SubwayMan <tyche4490@gmail.com>
2022-09-17 18:24:50 -0400

    scaffolding

--- homepage.py
+++ homepage.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file, redirect, send_from_directory, flash
 from test_filemod import samplefilemod
+from transformers import *
 from werkzeug.utils import secure_filename
 import string
 import os
@@ -23,11 +24,11 @@ def upload():
     file = request.files["file"]
     filename = ''.join(random.choices(string.ascii_letters + string.digits, k=16)) + secure_filename(file.filename)
     file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    stl_ify(filename)
     return {"filename": filename}, 200
 
 @app.route("/download/<name>", methods=["GET"])
 def download(name):
-    print("I find")
     return send_from_directory(app.config["UPLOAD_FOLDER"], name, as_attachment=True)
 
         
---
+++ transformers.py
@@ -0,0 +1,5 @@
+
+
+def stl_ify(filename: str):
+    """Function that takes in a LAS file and turns it into an STL file."""
+
