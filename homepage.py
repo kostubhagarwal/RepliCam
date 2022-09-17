a2dfaf329e02b37553ae6c3041ee05298ec42ac6
SubwayMan <tyche4490@gmail.com>
2022-09-17 13:58:29 -0400

    save to tmp approach

--- homepage.py
+++ homepage.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect
 from test_filemod import samplefilemod
+from werkzeug.utils import secure_filename
+import string
+import os
 import io
+import random
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 100 * 10E6
 app.config["UPLOAD_EXTENSIONS"] = [".las", ".txt"]
+app.config["UPLOAD_FOLDER"] = "/tmp"
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def homepage():
-    if request.method == "POST":
-        return send_file(samplefilemod(request.files["file"]), as_attachment=True, download_name="unnamed.txt", mimetype="text/plain")
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        flash("no file found")
+        return redirect(request.url)
+    file = request.files["file"]
+    filename = ''.join(random.choices(string.ascii_letters + string.digits, k=16)) + secure_filename(file.filename)
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return {"filename": filename}, 200
+
+
+        #return send_file(samplefilemod(request.files["file"]), as_attachment=True, download_name="unnamed.txt", mimetype="text/plain")
         
-    else:
-        return render_template("index.html")
--- static/index.js
+++ static/index.js
@@ -8,18 +8,18 @@ form.addEventListener("submit", function(e) {
     let file = document.querySelector('input[type="file"]');
     let data = new FormData();
     data.append("file", file.files[0]);
-    fetch("/", {
+    fetch("/upload", {
         method: "POST",
         body:data
     })
     .then(response => response.blob())
     .then(blob => {
-        var url = window.URL.createObjectURL(blob);
-        var a = document.createElement('a');
-        a.href = url;
-        a.download = "filename.xlsx";
-        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-        a.click();    
-        a.remove();  //after
-    );
+            var url = window.URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = "filename.xlsx";
+            document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+            a.click();    
+            a.remove();  //after
+    });
 });
